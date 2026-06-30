18bd587af94e34e5fcb6f019f031c1cc7ae7754c
ziadboutros <ziadboutros15@gmail.com>
2026-06-30 09:49:43 +0300

    Remove generated files from repository

--- venv/lib/python3.12/site-packages/opentelemetry/exporter/otlp/proto/common/_log_encoder.py
+++
@@ -1,9 +0,0 @@
-# Copyright The OpenTelemetry Authors
-# SPDX-License-Identifier: Apache-2.0
-
-
-from opentelemetry.exporter.otlp.proto.common._internal._log_encoder import (
-    encode_logs,
-)
-
-__all__ = ["encode_logs"]
